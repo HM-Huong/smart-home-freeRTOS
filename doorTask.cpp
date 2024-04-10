dcf795901658d04243e8bbec6244d5bfb08c97a1
HM-Huong <hmhuong1408@outlook.com>
2024-04-10 10:21:23 +0700

    add servo control code in doorTask

--- doorTask.cpp
+++ doorTask.cpp
@@ -3,6 +3,7 @@
 #include "Keypad.h"
 #include "global.h"
 #include "string.h"
+#include "Servo.h"
 
 static const byte ROWS = 4;
 static const byte COLS = 4;
@@ -13,6 +14,7 @@ static char KEYS[ROWS][COLS] = {
 	{'0', 'F', 'E', 'D'}};
 static const byte PIN_ROWS[ROWS] = {2, 3, 4, 5};
 static const byte PIN_COLS[COLS] = {6, 7, 8, 9};
+static const int SERVO_PIN = 10;
 
 static Keypad keypad = Keypad(makeKeymap(KEYS), (byte *)PIN_ROWS, (byte *)PIN_COLS, ROWS, COLS);
 static BaseType_t rc;
@@ -22,8 +24,19 @@ static const int MAX_LEN = 20;
 static char passwd[MAX_LEN] = "1234";
 static char input[MAX_LEN] = "";
 static byte input_len = 0;
+Servo servo;
+
+void inline openDoor() {
+	servo.write(0);
+}
+
+void inline closeDoor() {
+	servo.write(90);
+}
 
 void DoorOpeningTask(void *pvParameters) {
+	servo.attach(SERVO_PIN);
+
 	while (1) {
 		key = keypad.getKey();
 		if (key == NO_KEY) {
@@ -42,6 +55,7 @@ void DoorOpeningTask(void *pvParameters) {
 			input[input_len] = '\0';
 			if (strcmp(input, passwd) == 0) {
 				pcv = "Door opened";
+				openDoor();
 				buzzerPlay(4699, 100);
 				buzzerPlay(4699, 100);
 			} else {
@@ -50,6 +64,13 @@ void DoorOpeningTask(void *pvParameters) {
 			}
 			rc = xQueueSend(monitorQueue, &pcv, portMAX_DELAY);
 			input_len = 0;
+			break;
+		case 'F':
+			buzzerPlay(1000, 100);
+			closeDoor();
+			input_len = 0;
+			pcv = "Door closed";
+			rc = xQueueSend(monitorQueue, &pcv, portMAX_DELAY);
 			break;
 		case 'C':
 			buzzerPlay(1000, 100);
--- global.h
+++ global.h
@@ -6,12 +6,13 @@
 #include "semphr.h"
 
 #define BUZZER_PIN 13
-#define buzzerPlay(fre, dur) { \
+#define buzzerPlay(fre, dur)                        \
+	{                                               \
 		xSemaphoreTake(buzzerMutex, portMAX_DELAY); \
-		tone(BUZZER_PIN, fre, dur); \
-		xSemaphoreGive(buzzerMutex); \
-		delay(72); \
-}
+		tone(BUZZER_PIN, fre, dur);                 \
+		xSemaphoreGive(buzzerMutex);                \
+		delay(72);                                  \
+	}
 
 extern SemaphoreHandle_t buzzerMutex;
 extern QueueHandle_t monitorQueue;
