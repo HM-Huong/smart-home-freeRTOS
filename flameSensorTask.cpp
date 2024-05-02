9320972ff1d8a129515f613aa8d64386a8ad8f29
HM-Huong <hmhuong1408@outlook.com>
2024-05-02 22:37:03 +0700

    edit message of flameSensorTask

--- flameSensorTask.cpp
+++ flameSensorTask.cpp
@@ -5,19 +5,23 @@
 #include "printTask.h"
 
 static CloudData cloudData;
+static bool hasFlame;
 
 void flameSensorTask(void *pvParameters) {
 	pinMode(FLAME_SENSOR_PIN, INPUT);
 	cloudData.type = CloudData::MESSAGE;
-	cloudData.data.message = "Your house is safe now!";
-	xQueueSend(cloudQueue, &cloudData, 0);
 
 	while (1) {
 		if (digitalRead(FLAME_SENSOR_PIN) == LOW) {
+			hasFlame = true;
 			lcdPrint("Flame detected", 0, 0, 0);
 			cloudData.data.message = "Flame detected";
-			sendOpenDoorEvent();
+			xQueueSend(cloudQueue, &cloudData, 0);
 			buzzerPlay(3951, 1000);
+			sendOpenDoorEvent();
+		} else if (hasFlame) {
+			hasFlame = false;
+			cloudData.data.message = "Your house is safe now!";
 			xQueueSend(cloudQueue, &cloudData, 0);
 		}
 		delay(1000);
